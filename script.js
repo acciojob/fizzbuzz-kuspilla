869bcd1e69091f4d04d7a0f9b12544c0a106c3c5
Dasthu <118829965+kuspilla@users.noreply.github.com>
2023-11-25 06:46:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
 //your JS code here. If required.
+let output = '';
+
+for (let i = 1; i <= 20; i++) {
+  if (i % 3 === 0 && i % 5 === 0) {
+    output += 'FizzBuzz';
+  } else if (i % 3 === 0) {
+    output += 'Fizz';
+  } else if (i % 5 === 0) {
+    output += 'Buzz';
+  } else {
+    output += i.toString();
+  }
+
+  // Add a newline character ("\n") after each number/string
+  output += '\n';
+}
+
+// Print the final output using alert
+alert(output);
